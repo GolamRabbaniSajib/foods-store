586556ca47a025e791e1f336e74521770fd6d730
sbsajibsx <golamrobbanisajib@gmail.com>
2024-12-22 18:32:50 +0600

    set up all routers

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,6 +3,12 @@ import Main from "../layOut/Main";
 import Error from "../pages/Error";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import Home from "../pages/Home";
+import AddFood from "../pages/AddFood";
+import MyFoods from "../pages/MyFoods";
+import MyOrders from "../pages/MyOrders";
+import Gallery from "../pages/Gallery";
+import AllFoods from "../pages/AllFoods";
 
 
 const router = createBrowserRouter([
@@ -18,6 +24,30 @@ const router = createBrowserRouter([
             {
                 path: '/register',
                 element: <Register></Register>
+            },
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/add-food',
+                element: <AddFood></AddFood>
+            },
+            {
+                path: '/my-foods',
+                element: <MyFoods></MyFoods>
+            },
+            {
+                path: '/my-orders',
+                element: <MyOrders></MyOrders>
+            },
+            {
+                path: '/gallery',
+                element: <Gallery></Gallery>
+            },
+            {
+                path: '/foods',
+                element: <AllFoods></AllFoods>
             }
         ]
     }
